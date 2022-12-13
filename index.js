471436de535217a3245d79f89021b1a1201eccd0
thomas <tom.angotti11@gmail.com>
2022-12-13 10:09:32 -0700

    commit tree 31f3a45126dbdaeb393a87cccca9a35da43b7aa9

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase();
+}
+var bestCustomer;
+function setBestCustomer() {
+   
+   return bestCustomer = 'not bob';
+};
+
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = '';
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'tom';
+}
